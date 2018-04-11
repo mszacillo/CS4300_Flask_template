4dfa48db20677b841b9effb95aff072a846c05df
Michael Szacillo <mszacillo@dhcp-rhodes-1602.redrover.cornell.edu>
2018-04-11 19:11:31 -0400

    Search

---
+++ SearchTest.py
@@ -0,0 +1,40 @@
+from Similarity import *
+
+with open("songData.json", "r") as f:
+	song_transcripts = json.load(f)
+
+songs = [song_transcripts[index] for index in song_transcripts]
+
+start_time = time.time()
+tokenized_songs = tokenize_transcript(songs)
+execution_time = time.time() - start_time
+
+print("Tokenization took %f seconds." % execution_time)
+n_songs = len(tokenized_songs)
+
+start_time = time.time()
+inv_idx = build_inverted_index(tokenized_songs)
+execution_time = time.time() - start_time
+
+print("Inverted index took %f seconds." % execution_time)
+
+start_time = time.time()
+idf = compute_idf(inv_idx, n_songs)
+execution_time = time.time() - start_time
+
+print("IDF took %f seconds." % execution_time)
+
+start_time = time.time()
+doc_norms = computer_doc_norms(inv_idx, idf, n_songs)
+execution_time = time.time() - start_time
+
+print("Norm took %f seconds." % execution_time)
+
+start_time = time.time()
+scores = song_search("Please go fuck yourself", inv_idx, idf, doc_norms)
+execution_time = time.time() - start_time
+
+print("Song search took %f seconds." % execution_time)
+
+for score, idx in scores[0:10]:
+	print("%s with a score of %f" % (songs[idx]["title"], score))
---
+++ Similarity.py
@@ -0,0 +1,94 @@
+import re
+import json
+import time
+import math
+from nltk.tokenize import TreebankWordTokenizer
+
+import numpy as np
+
+tokenizer = TreebankWordTokenizer()
+
+def tokenize_transcript(transcripts):
+	for idx, song in enumerate(transcripts):
+		lyrics = song["lyrics"]
+		lyrics = lyrics.replace("\\n", " ").replace("[Hook", " ").replace("[Verse", " ").replace("b", " ", 1)
+		transcripts[idx]["lyrics"] = re.findall(r"[a-z]+", lyrics.lower())
+
+	return transcripts
+
+def build_inverted_index(songs):
+	indexdict = {}
+
+	for i,x in enumerate(songs):
+		counter_dict = {}
+		for word in x["lyrics"]:
+			if word in counter_dict:
+				counter_dict[word] +=1
+			else:
+				counter_dict[word] =1
+		for term,freq in counter_dict.items():
+			if term not in indexdict:
+				indexdict[term] = []
+			indexdict[term].append((i,counter_dict[term]))
+
+	return indexdict
+
+def compute_idf(inv_idx, n_songs, min_df=10, max_df_ratio=0.95):
+	idf = {}
+
+	for i in inv_idx:
+		if((len(inv_idx[i]) < min_df) or (float(len(inv_idx[i]))/n_songs > max_df_ratio)):
+			continue
+		else:
+			#calculate log base 2
+			idf[i] = math.log(float(n_songs)/(1 + len(inv_idx[i]))) / math.log(2)
+
+	return idf
+
+def computer_doc_norms(inv_idx, idf, n_songs):
+
+	norms = np.zeros(n_songs)
+
+	for term in idf:
+		for doc_id, tf in inv_idx[term]:
+			norms[doc_id] += (tf*idf[term])**2
+
+	return np.sqrt(norms)
+
+def song_search(query, index, idf, doc_norms):
+
+	querytokens = tokenizer.tokenize(query)
+	uniquetokens = np.unique(querytokens)
+
+	temp = np.zeros(len(doc_norms))
+	qnorm = 0
+	for word in uniquetokens:
+		if word in idf:
+			qnorm = qnorm + (idf[word]*querytokens.count(word))**2
+		else:
+			continue
+
+	for word in uniquetokens:
+		if word not in index:
+			continue
+		else:
+			qtf = querytokens.count(word)
+			for doc, tf in index[word]:
+				if word in idf:
+					temp[doc] = temp[doc] + ((tf * qtf)*(idf[word]**2))
+
+	qnorm = math.sqrt(qnorm)
+	temp = np.divide(temp, qnorm)
+
+	results = []
+
+	for idx, dnorm in enumerate(doc_norms):
+		if(dnorm != 0):
+			results.append(((temp[idx]/dnorm), idx))
+		else:
+			results.append(((temp[idx]), idx))
+	
+	return sorted(results, key=lambda x: x[0], reverse=True)		
+
+
+
