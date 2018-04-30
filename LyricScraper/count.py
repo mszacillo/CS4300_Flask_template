54213a29c1aaec621fbeb68aa6dce83a626b06c0
Michael Szacillo <mszacillo@dhcp-rhodes-2909.redrover.cornell.edu>
2018-04-30 13:12:25 -0400

    newdata

--- LyricScraper/count.py
+++ LyricScraper/count.py
@@ -2,9 +2,23 @@ import json
 with open("concatenated.json", "r") as f:
 	songs2 = json.load(f)
 
-artists = []
-for song in songs2:
-	if song["artist"] not in artists:
-		artists.append(song["artist"])
+noduplicates = []
+songartists = []
 
-print(sorted(artists))
+for song in songs2:
+	if song["artist"] not in songartists:
+		songartists.append(song["artist"])
+
+print(sorted(songartists))
+
+# print(len(songs2))
+# for song in songs2:
+# 	currentsong = song["artist"] + song["title"]
+# 	if currentsong not in songartists:
+# 		noduplicates.append(song)
+
+#print(sorted(artists))
+# print(len(noduplicates))
+
+# with open("concatenated.json", "w") as outfile:
+# 		json.dump(songs2, outfile)
